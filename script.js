d29d9ffdd8038193772060476b411f9639c6cde0
kaskad <matzkel@proton.me>
2023-12-09 13:45:45 +0200

    Add function that creates grid

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+function createGrid(parentElement, rows, columns) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid-container");
+
+    for (let i = 0; i < columns; i++) {
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
+        for (let j = 0; j < rows; j++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid-element");
+
+            gridColumn.appendChild(gridElement);
+        }
+        grid.appendChild(gridColumn);
+    }
+
+    parentElement.appendChild(grid);
+}
